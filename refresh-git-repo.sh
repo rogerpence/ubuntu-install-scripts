6ec54510e8ec1d8d6da9a6c2df7420106f243dff
Roger Pence <roger.pence@gmail.com>
2019-03-17 00:36:27 -0500

    2019-03-17.00:36:27

--- refresh-git-repo.sh
+++ refresh-git-repo.sh
@@ -3,6 +3,23 @@
 today=`date +%Y-%m-%d.%H:%M:%S`
 
 mkdir vscode -p
+mkdir vscode/snippets -p
+mkdir root -p
+mkdir git -p
+
+# Copy Visual Studio Code settings file. 
 cp ~/.config/Code/User/settings.json ./vscode
+cp ~/.config/Code/User/snippets/*.json ./vscode/snippets
+
+# Copy Bash/zsh dot files. 
+cp ~/.rp_bashrc ./root
+cp ~/.bashrc ./root
+cp ~/.zshrc ./root
+
+# Copy Git files.
+cp ~/.gitconfig ./git 
 
 git cm $today
+git push
+
+
